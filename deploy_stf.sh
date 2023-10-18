933c89599f2f7bd4a3be571e3324bb3649ebde4a
cupingz <64177319+cupingz@users.noreply.github.com>
2023-10-18 23:37:51 +0700

    Update deploy_stf.sh

--- deploy_stf.sh
+++ deploy_stf.sh
@@ -13,16 +13,16 @@
 # 
 #######################################################################################
 
-NETNAME=""
+NETNAME="eth0"
 NETWORK_INTERFACES=$(ls /sys/class/net)
 
-DNS_ADDRESS=$(nmcli device show ${NETNAME}|grep "IP4\.DNS\[1\]"|awk '{print $2}')
+DNS_ADDRESS=$(nmcli device show ${NETNAME}|grep "IP4\.ADDRESS\[1\]"|awk '{print $2}')
 echo "DNS Address: ${DNS_ADDRESS}"
 
 # Get exported IP Adreess 
-[ ! -z "$(echo ${NETWORK_INTERFACES} | grep "wlo1")" ]&&NETNAME="wlo1"
-[ ! -z "$(echo ${NETWORK_INTERFACES} | grep "eno1")" ]&&NETNAME="eno1"
-IP_ADDRESS=$(ifconfig ${NETNAME}|grep "inet "|awk -F: '{print $2}'|awk '{print $1}')
+# [ ! -z "$(echo ${NETWORK_INTERFACES} | grep "wlo1")" ]&&NETNAME="wlo1"
+# [ ! -z "$(echo ${NETWORK_INTERFACES} | grep "eno1")" ]&&NETNAME="eno1"
+IP_ADDRESS=$(ifconfig ${NETNAME}|grep "inet "|awk '{print $2}'|awk '{print $1}')
 [ $# -gt 0 ]&&IP_ADDRESS=$1
 echo "IP ADDRESS: ${IP_ADDRESS}"
 
